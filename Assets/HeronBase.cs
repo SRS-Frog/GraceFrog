17696b023f019f721870a2ac4353e0072d606230
Caleb Kim <calebmk21@gmail.com>
2023-02-27 00:45:50 -0800

    state machine testing

---
+++ Assets/HeronBase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeronBase : StateMachineBehaviour
+{
+    public float speed = 1.5f;
+    public float riseRange = 3f;
+    Transform player;
+    Rigidbody2D rb;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 target = new Vector2(player.position.x, rb.position.y + 5f);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+
+
+        if (Vector2.Distance(player.position,rb.position) > riseRange)
+        {
+            animator.SetTrigger("Rise");
+            rb.MovePosition(newPos);
+        }
+        if (Vector2.Distance(player.position,rb.position) <= riseRange)
+        {
+            animator.SetTrigger("Idle");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
